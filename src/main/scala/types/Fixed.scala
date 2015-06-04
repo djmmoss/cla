7933bbe09b45e177d5ad32bb859ecc8318efe48f
Duncan Moss <djm.moss@gmail.com>
2015-06-04 17:35:06 +1000

    started working on fixed

--- src/main/scala/types/Fixed.scala
+++ src/main/scala/types/Fixed.scala
@@ -6,29 +6,27 @@ import ChiselError._
 
 object Fixed {
 
-    def apply(x : Int) : UInt = apply(BigInt(x))
-    def apply(x : Int, width : Int) : UInt = apply(BigInt(x), width)
-    def apply(x : BigInt) : UInt = Lit(checkSign(x)){UInt()};
-    def apply(x : BigInt, width : Int) : UInt = Lit(checkSign(x), width){UInt()};
-    def apply(x : String) : UInt = Lit(x, -1){UInt()};
-    def apply(x : String, width : Int) : UInt = Lit(x, width){UInt()};
-    def apply(x: String, base: Char): UInt = Lit(x, base, -1){UInt()};
-    def apply(x: String, base: Char, width: Int): UInt = Lit(x, base, width){UInt()};
-    def apply(x: Node): UInt = UInt(x, -1)
-    //def apply(x: Node, width: Int): UInt = UInt(width = width).asTypeFor(x)
+    def apply(x : Int) : Fixed = apply(BigInt(x))
+    def apply(x : Int, width : Int) : Fixed = apply(BigInt(x), width)
+    def apply(x : BigInt) : Fixed = Lit(x){Fixed()};
+    def apply(x : BigInt, width : Int) : Fixed = Lit(x, width){Fixed()};
 
-    def apply(dir : IODirection = null, width : Int = -1) : UInt = {
-        val res = new UInt();
+    def apply(dir : IODirection = null, width : Int = -1) : Fixed = {
+        val res = new Fixed();
         res.create(dir, width)
         res
     }
-    def DC(width: Int): UInt = Lit("b" + "?"*width, width){UInt()}
-    
-    private def checkSign(x: BigInt) = {
-        if (x < 0)
-            ChiselError.error("UInt can't represent negative literal " + x)
-        x
-    }
 }
 
-class Fixed extends UInt{}
+class Fixed extends Bits with Num[Fixed] {
+    type T = Fixed
+
+    /* Fixed Factory Method */
+    override def fromNode(n : Node): this.type = {
+        val res = Fixed(OUTPUT).asTypeFor(n).asInstanceOf[this.type]
+        res
+    }
+
+
+
+}
